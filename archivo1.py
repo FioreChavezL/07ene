21b239f0e7a1a6997b4dea6edebaaa189b43ec79
Fiorella_Python <fiorechavezliv@gmail.com>
2024-01-07 12:55:30 -0500

    Creado archivo1.py y archivo2.py

---
+++ archivo1.py
@@ -0,0 +1,5 @@
+import datetime as dt
+
+ahora = dt.datetime.now()
+
+print(f"Estamos {ahora.day} del mes {ahora.month} del {ahora.year}")
---
+++ archivo2.py
@@ -0,0 +1,4 @@
+import random
+
+numeroale = random.randint(1,5)
+print(f"El número aleatorio es {numeroale}")
